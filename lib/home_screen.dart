c3392a56ecedac04b4375acd310b6fea76af85dc
fakhruddin07 <fakhruddinnoman95@gmail.com>
2023-07-07 22:01:18 +0600

    add floating action button

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -15,26 +15,33 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text("CRUD App Using REST API"),
         centerTitle: true,
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
       body: ListView.separated(
         itemCount: 5,
         separatorBuilder: (BuildContext context, int index) {
           return const Divider();
         },
         itemBuilder: (context, index) {
-        return ListTile(
-          leading: Icon(Icons.image, size: 32,),
-          title: Text("Product Name"),
-          subtitle: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text("Product Code"),
-              Text("Total Price"),
-              Text("Available Unit"),
-            ],
-          ),
-          trailing: Text("Unit Price"),
-        );
-      },
+          return ListTile(
+            leading: Icon(
+              Icons.image,
+              size: 32,
+            ),
+            title: Text("Product Name"),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Product Code"),
+                Text("Total Price"),
+                Text("Available Unit"),
+              ],
+            ),
+            trailing: Text("Unit Price"),
+          );
+        },
       ),
     );
   }
