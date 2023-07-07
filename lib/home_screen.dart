075734867c8493c9b9c39e42b277681e9f168640
fakhruddin07 <fakhruddinnoman95@gmail.com>
2023-07-07 20:53:50 +0600

    create a listView Builder and ListTile

---
+++ lib/home_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("CRUD App Using REST API"),
+        centerTitle: true,
+      ),
+      body: ListView.separated(
+        itemCount: 5,
+        separatorBuilder: (BuildContext context, int index) {
+          return const Divider();
+        },
+        itemBuilder: (context, index) {
+        return ListTile(
+          leading: Icon(Icons.image, size: 32,),
+          title: Text("Product Name"),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Product Code"),
+              Text("Total Price"),
+              Text("Available Unit"),
+            ],
+          ),
+          trailing: Text("Unit Price"),
+        );
+      },
+      ),
+    );
+  }
+}
